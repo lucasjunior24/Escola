20a60e10e716b961d9ffc4c66eae99ba5aa379a4
lucasjunior24 <lukateku@gmail.com>
2021-11-06 01:02:24 -0300

    Criado modal para adicionar Aluno

--- src/App.js
+++ src/App.js
@@ -7,12 +7,46 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const baseUrl = "https://localhost:44319/api/alunos";
+
   const [data, setData] = useState([]);
+
+  const [modalIncluir, setModalIncluir] = useState(false);
+
+  const [alunoSelecionado, setAlunoSelecionado] = useState({
+    id: '',
+    nome: '',
+    email: '',
+    idade: ''
+  });
+
+  const abrirFecharModalIncluir = () => {
+    setModalIncluir(!modalIncluir)
+  }
+
+  const handleChange = e => {
+    const {name, value} = e.target;
+    setAlunoSelecionado({
+      ...alunoSelecionado, [name]:value
+    });
+    console.log(alunoSelecionado);
+  }
 
   const pedidoGet = async() => {
     await axios.get(baseUrl)
       .then(response => {
         setData(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  const pedidoPost = async() => {
+    delete alunoSelecionado.id;
+    alunoSelecionado.idade = parseInt(alunoSelecionado.idade);
+    await axios.post(baseUrl, alunoSelecionado)
+      .then(response => {
+        setData(data.concat(response.data));
+        abrirFecharModalIncluir();
       }).catch(error => {
         console.log(error);
       })
@@ -27,7 +61,7 @@ function App() {
       <br />
       <h3>Cadastro de alunos</h3>
       <header>
-        <button className='btn btn-success'>Novo Aluno</button>
+        <button className='btn btn-success' onClick={() => abrirFecharModalIncluir()}>Novo Aluno</button>
       </header>
       <table>
         <thead>
@@ -47,13 +81,36 @@ function App() {
               <td>{aluno.email}</td>
               <td>{aluno.idade}</td>
               <td>
-                <button className="btn btn-primary">Editar</button>
+                <button className="btn btn-primary">Editar</button> {"  "}
                 <button className="btn btn-danger">Excluir</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+      <Modal isOpen={modalIncluir} >
+        <ModalHeader>Incluir Aluno</ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Nome: </label>
+            <br />
+            <input type="text" className='form-control' name="nome" onChange={handleChange} />
+            <br />
+            <label>Email: </label>
+            <br />
+            <input type="text" className='form-control' name="email" onChange={handleChange} />
+            <br />
+            <label>Idade: </label>
+            <br />
+            <input type="text" className='form-control' name="idade" onChange={handleChange} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+         <button className="btn btn-primary" onClick={() => pedidoPost()}>Incluir</button> {"  "}
+         <button className="btn btn-danger" onClick={() => abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
